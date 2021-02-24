d89f478452f69e4e6b92d8f917d11ffeb93a7ee1
Justin Wyrobek <justin.wyrobek@gmail.com>
2021-02-24 01:23:07 -0500

    Add messages

---
+++ mixedMessages.js
@@ -0,0 +1,18 @@
+let verb = ["can", "should", "want to"];
+let message = ["let go of the past",
+               "forgive your enemies",
+               "be and do your best",
+               "trust yourself",
+               "listen to your instincts",
+               "believe in yourself",
+               "stay true to yourself",
+               "be faithful to your family and friends",
+               "invest in yourself",
+               "consider your dreams",
+               "reconnect with old friends",
+               "take care of your body",
+               "be gentle with your mistakes",
+               "embrace silence",
+               "learn that hobby you always wanted to",
+               "have courage in all things",
+               "be the hero you always looked up to"];
